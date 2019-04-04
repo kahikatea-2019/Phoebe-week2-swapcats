3e23facb35380b9ca1af9175653d7b55b1f4b450
Eteroa Lafaele <eteroalafaele080@gmail.com>
2019-04-05 11:59:19 +1300

    cretaed migrations and seeds

--- migrations/20190404175309_users.js
+++ migrations/20190404175309_users.js
@@ -1,8 +1,12 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('email')
+    table.string('password')
+  })
+}
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('users')
+}
--- migrations/20190405095048_products.js
+++ migrations/20190405095048_products.js
@@ -1,8 +1,13 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('products', (table) => {
+    table.increments('id').primary()
+    table.integer('user_id').references('users_id')
+    table.string('product_category')
+    table.string('product_description')
+    table.string('img_url')
+  })
+}
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('products')
+}
