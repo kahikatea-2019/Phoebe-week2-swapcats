428d4ba6e6cf586d217020591456f72e3a3eb1f4
Kuan Thai Lim <philip.limkt@gmail.com>
2019-04-05 14:28:23 +1300

    adding working pages

--- routes/trades.js
+++ routes/trades.js
@@ -1,127 +1,130 @@
 const express = require('express')
 const db = require('../db')
+
 const router = express.Router()
-// router.get('/', (req, res) => {
-//   res.send('It work')
-// })
+
 router.get('/', (req, res) => {
-  res.redirect('/trades')
+  res.render('homepage')
 })
 
-router.get('/trades', (req, res) => {
-  res.render('main')
-})
+// router.get('/trades', (req, res) => {
+//   res.render('/homepage')
+// })
 
-router.get('/trades/shoes', (req, res) => {
+// router.get('/trades', (req, res) => {
+//   res.render('main')
+// })
+
+router.get('/shoes', (req, res) => {
   db.getShoes()
     .then(shoes => {
       res.render('shoes', shoes)
     })
 })
 
-router.get('/trades/phones', (req, res) => {
+router.get('/electronics', (req, res) => {
   db.getPhones()
     .then(phones => {
-      res.render('phones', phones)
+      res.render('electronics', phones)
     })
 })
 
-router.get('/trades/bags', (req, res) => {
+router.get('/random', (req, res) => {
   db.getBags()
     .then(bags => {
-      res.render('bags', bags)
+      res.render('random', bags)
     })
 })
 
-// Add New Product
+// // Add New Product
 
-router.get('/trades/newproduct', (req, res) => {
-  res.render('newproduct')
-})
+// router.get('/trades/newproduct', (req, res) => {
+//   res.render('newproduct')
+// })
 
-router.post('/trades/newproduct/submit', (req, res) => {
-  const newProduct = {
-    product_category: req.body.product_category,
-    product_description: req.body.product_description,
-    img_url: req.body.img_url,
-    user_id: req.body.user_id
-  }
+// router.post('/trades/newproduct/submit', (req, res) => {
+//   const newProduct = {
+//     product_category: req.body.product_category,
+//     product_description: req.body.product_description,
+//     img_url: req.body.img_url,
+//     user_id: req.body.user_id
+//   }
 
-  db.addProduct(newProduct)
+//   db.addProduct(newProduct)
 
-    .then(() => res.redirect('/trades'))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+//     .then(() => res.redirect('/trades'))
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
-// Add New User
-router.get('/trades/newuser', (req, res) => {
-  res.render('newuser')
-})
+// // Add New User
+// router.get('/trades/newuser', (req, res) => {
+//   res.render('newuser')
+// })
 
-router.post('/trades/newuser/submit', (req, res) => {
-  const newUser = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }
+// router.post('/trades/newuser/submit', (req, res) => {
+//   const newUser = {
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+//   }
 
-  db.addProduct(newUser)
-    .then(() => res.redirect('/trades'))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+//   db.addProduct(newUser)
+//     .then(() => res.redirect('/trades'))
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
-// Edit User
+// // Edit User
 
-router.get('/users/:id', (req, res) => {
-  const id = req.params.id
-  db.getUser(id)
-    .then(user => res.render('editUser', user))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+// router.get('/users/:id', (req, res) => {
+//   const id = req.params.id
+//   db.getUser(id)
+//     .then(user => res.render('editUser', user))
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
-router.post('/users/submit/:id', (req, res) => {
-  const id = req.params.id
-  const newProfile = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }
+// router.post('/users/submit/:id', (req, res) => {
+//   const id = req.params.id
+//   const newProfile = {
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+//   }
 
-  db.editUser(newProfile, id)
-    .then(() => res.redirect('/users'))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+//   db.editUser(newProfile, id)
+//     .then(() => res.redirect('/users'))
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
-// Edit Product
-router.get('/products/:id', (req, res) => {
-  const id = req.params.id
-  db.getProduct(id)
-    .then(product => res.render('editProduct', product))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
-router.post('/products/submit/:id', (req, res) => {
-  const id = req.params.id
-  const newProductInfo = {
-    product_category: req.body.product_category,
-    product_description: req.body.product_description,
-    img_url: req.body.img_url,
-    user_id: req.body.user_id
-  }
-  db.editUser(newProductInfo, id)
-    .then(() => res.redirect('/trades'))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR: ' + err.message)
-    })
-})
+// // Edit Product
+// router.get('/products/:id', (req, res) => {
+//   const id = req.params.id
+//   db.getProduct(id)
+//     .then(product => res.render('editProduct', product))
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
+// router.post('/products/submit/:id', (req, res) => {
+//   const id = req.params.id
+//   const newProductInfo = {
+//     product_category: req.body.product_category,
+//     product_description: req.body.product_description,
+//     img_url: req.body.img_url,
+//     user_id: req.body.user_id
+//   }
+//   db.editUser(newProductInfo, id)
+//     .then(() => res.redirect('/trades'))
+//     .catch(err => {
+//       res.status(500).send('DATABASE ERROR: ' + err.message)
+//     })
+// })
 
 module.exports = router
